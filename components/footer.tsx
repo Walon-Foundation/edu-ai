27b23d673bee8c93be538efa4f2fc0b6c56f47ea
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-11-17 11:36:25 +0000

    update the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-900 text-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="grid md:grid-cols-4 gap-8">
           {/* Company Info */}
           <div className="md:col-span-2">
@@ -36,7 +36,7 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
+        <div className="border-t border-gray-800 mt-4 pt-6 text-center text-gray-400">
           <p>&copy; 2024 EduAI. All rights reserved.</p>
         </div>
       </div>
