91698aff1b50c7eaa8dcf590b10bb6e0400b6f99
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-11-18 22:48:57 +0000

    add validation for the databas e url

--- lib/env.ts
+++ lib/env.ts
@@ -5,7 +5,8 @@ const envSchema = z.object({
     CLERK_SECRET_KEY:z.string({ error:"CLERK_SECRET_KEY is required"}),
     SUPABASE_URL:z.string({ error: "SUPABASE_URL is required"}),
     SUPABASE_ROLE_KEY:z.string({ error: "SUPABASE_ROLE_KEY is required"}),
-    NODE_ENV:z.string({ error: "NODE_ENV is required"})
+    NODE_ENV:z.string({ error: "NODE_ENV is required"}),
+    DATABASE_URL:z.string({ error:"DATABASE_URL is required"})
 })
 
 export const env = envSchema.parse({
@@ -13,5 +14,6 @@ export const env = envSchema.parse({
     CLERK_SECRET_KEY:process.env.CLERK_SECRET_KEY,
     SUPABASE_URL:process.env.SUPABASE_URL,
     SUPABASE_ROLE_KEY:process.env.SUPABASE_ROLE_KEY,
-    NODE_ENV:process.env.NODE_ENV
+    NODE_ENV:process.env.NODE_ENV,
+    DATABASE_URL:process.env.DATABASE_URL
 })
